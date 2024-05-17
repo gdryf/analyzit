319c0e1440c4b69c8d44e2e740b6098c62807a46
Gillian <160870793+gdryf@users.noreply.github.com>
2024-05-17 20:41:55 +0200

    Update test_functions.py

--- package/tests/test_functions.py
+++ package/tests/test_functions.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from src.analyzit_cosmetics import search_ingredients
+from src.analyzit_cosmetics import search_ingredients, danger_list, amount_dangers
 def test_search_ingredients():
     path_to_database1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'database_products.csv'))
     # Test 1 : if the barcode is found
@@ -9,7 +9,7 @@ def test_search_ingredients():
     assert search_ingredients('999999', path_to_database1) == "No ingredients found for this barcode.",  "Test 2 failed "
     # Test 3 : if another error occurs ( file not found or search unsucessful)
     assert search_ingredients('123456', 'inexistant_file.csv') == "An error occurred while loading the file or during the search : [Errno 2] No such file or directory: 'inexistant_file.csv'",  "Test 3 failed "
-from src.analyzit_cosmetics import danger_list
+    
 def test_danger_list():
     path_to_database1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'database_products.csv'))
     path_to_database2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'database_dangers.csv'))
@@ -22,15 +22,13 @@ def test_danger_list():
     # Test 4 : if one of the csv files isn't found : 
     assert danger_list ('999999', 'inexistant_file1.csv','inexistant_file2.csv') == "An error occurred while loading the file or during the search : [Errno 2] No such file or directory: 'inexistant_file1.csv'", "Test 4 failed "
 
-from src.analyzit_cosmetics import amount_dangers
-
 def test_amount_dangers():
     dangerous_ingredients = {'stearic acid': ['Carcinogenic'], 'glycerin': ['Carcinogenic'], 'methylparaben': ['Paraben'], 'propylparaben': ['Paraben'], 'butylparaben': ['Carcinogenic, Paraben'], 'benzyl alcohol': ['Carcinogenic'], 'coumarin': ['Carcinogenic'], 'test' : ['Endocrine']}
     # Test 1 : if the user decides to apply the same order of priority for all the types of danger
     assert amount_dangers (dangerous_ingredients,3,3,3) == {'Paraben': 3, 'Carcinogenic': 4, 'Endocrine': 1}, "Test 1 failed"
     # Test 2 : if the user decides to apply different order of priorities, prioritizes the count of the most important type of danger
     assert amount_dangers (dangerous_ingredients,2,5,4) == {'Paraben': 2, 'Carcinogenic': 5, 'Endocrine': 1}, "Test 2 failed"
-    # Test 3 : if the minimum grade is given to "Carcinogenic" and maximum garde to "Endocrine"
+    # Test 3 : other test to complete coverage (if the minimum grade is given to "Carcinogenic" and maximum garde to "Endocrine")
     assert amount_dangers (dangerous_ingredients,4,1,5) == {'Paraben': 3, 'Carcinogenic': 4, 'Endocrine': 1}, "Test 3 failed"
-    # Test 4 : if the medium grade is given to "Carcinogenic" and minimum grade given to "Endocrine"
+    # Test 4 : other test to complete coverage (if the medium grade is given to "Carcinogenic" and minimum grade given to "Endocrine")
     assert amount_dangers (dangerous_ingredients,4,2,1) =={'Paraben': 3, 'Carcinogenic': 4, 'Endocrine': 1}, "Test 4 failed"
