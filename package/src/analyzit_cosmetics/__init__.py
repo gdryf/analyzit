b11589b4a74244f05ad34787e9ca5846178df77e
Gillian <160870793+gdryf@users.noreply.github.com>
2024-05-19 20:33:40 +0200

    Update __init__.py

--- package/src/analyzit_cosmetics/__init__.py
+++ package/src/analyzit_cosmetics/__init__.py
@@ -1,4 +1,4 @@
 """Analysis of the composition of cosmetics and tracking of how dangerous are the products used by the user.."""
 
-from .functions import search_ingredients, danger_list, amount_dangers, coefficient,grading, commentary, initialize_graph_grades, graph_grades
+from .functions import search_ingredients, danger_list, amount_dangers, coefficient,grading, commentary, graph_grades
 __version__ = "0.0.1"
